63c15ec826b0e2c972a0385a6d052322473fe28a
tanvirUpol <tanvirrahmanupol@gmail.com>
2023-09-24 17:12:04 +0600

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import CoordinatePage from './pages/CoordinatePage'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Files from './pages/files'
+import Files from './pages/Files'
 
 const router = createBrowserRouter([
   {
